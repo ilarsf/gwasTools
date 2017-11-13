a698adecd67e73fa5892db6af1a76d7cc0b00638
Lars Fritsche <ilarsf@gmail.com>
2017-11-13 13:19:10 +0100

    Add files via upload
    Rscripts for plotting GWAS results

---
+++ ManhattanPlot.r
@@ -0,0 +1,230 @@
+options(stringsAsFactors=F)
+library("plotrix")
+library("data.table")
+library("RColorBrewer")
+library("optparse")
+
+option_list <- list(
+  make_option("--input", type="character", default="",
+    help="Input file, tab delimited"),   
+  make_option("--prefix", type="character", default="",
+    help="Prefix of output files"),   
+  make_option("--top.size", type="numeric", default=0.125,
+    help="top size = proportion of total length y axis [default=0.125]"),
+  make_option("--break.top", type="numeric", default=15,
+    help="set axis break at -log10(P) [default=15]"),
+  make_option("--mh.width", type="numeric", default=1600,
+    help="Width Manhattan plot in pixel [default=1600]"),
+  make_option("--mh.height", type="numeric", default=900,
+    help="Height Manhattan plot in pixel [default=900]"),
+  make_option("--pointsize", type="numeric", default=16,
+    help="Point size of plots [default=16]"),
+  make_option("--hitregion", type="character", default="",
+    help="File with candidate regions, CHROM;START;END;COL;LEGENDTEXT [default='']"),
+  make_option("--chr", type="character", default="",
+    help="name of column with chromosome [default='CHR']"),
+  make_option("--pos", type="character", default="",
+    help="name of column with position [default='']"),
+  make_option("--p", type="character", default="",
+    help="name of column with p.value [default='']"),
+  make_option("--log10p", type="logical", default=T,
+    help="Input p.value column with -log10(p.value) [default=T]"),    
+  make_option("--maintitle", type="character", default="",
+    help="Plot title")  
+)
+
+parser <- OptionParser(usage="%prog [options]", option_list=option_list)
+
+args <- parse_args(parser, positional_arguments = 0)
+opt <- args$options
+print(opt)
+
+if (opt$hitregion != ""){
+	candidateRegions <- read.table(opt$hitregion,sep="\t",header=T,check.names=F)
+} else {
+	candidateRegions <- data.frame(
+		'CHROM'=character(0),
+		'START'=numeric(0),
+		'END'=numeric(0),
+		'COL'=character(0),
+		'LEGENDTEXT'=character(0)
+		)
+}
+
+chrcol <- opt$chr
+poscol <- opt$pos
+
+# horizontal lines and corresponding colors
+yLine <- c(-log10(5E-8))
+colLine <- c("red")
+
+gwas <- fread(opt$input)
+
+if(!opt$log10p) {
+	gwas$log10P <- -log10(gwas[[opt$p]])
+	ycol <- "log10P"
+} else {
+	ycol <- opt$p
+}
+
+gwas <- na.omit(data.frame(gwas[,c(chrcol,poscol,ycol),with=F]))
+Nmarkers <- dim(gwas)[1]
+
+# Thinning; remove 95% of variants with P > 0.05
+prethin1 <- which(gwas[[ycol]] >= -log10(0.05) & gwas[[ycol]] <= 6)
+prethin2 <- which(gwas[[ycol]] < -log10(0.05))
+prethin2 <- prethin2[order(rnorm(length(prethin2)))[1:(length(prethin2)/20)]]
+prethin <- c(prethin1,prethin2)
+
+# Additional thinning using unique after rounding position and p-value
+thinned <- prethin[which(!duplicated(data.frame(gwas[[chrcol]][prethin],
+				round(gwas[[poscol]][prethin]/10000),round(gwas[[ycol]][prethin],1))))]
+
+# Plot all SNPs with p < 1E-6
+keepTop <- which(gwas[[ycol]] > 6)
+thinned <- sort(c(keepTop,thinned))
+
+# Prepare plot data / two-colored chromosomes / with fixed gap
+CHR <- gwas[[chrcol]][thinned]
+CHR[which(CHR == 23)] <- "X"
+POS <- gwas[[poscol]][thinned]
+log10P <- gwas[[ycol]][thinned]
+chrs <- c(1:22,"X",23,"Y",24,"XY",25,"MT",26)
+chrs <- chrs[which(chrs %in% CHR)]
+chrNr <- as.numeric(as.character(factor(CHR,levels=chrs,labels=1:length(chrs))))
+chrColors <- c("grey40","grey60")[(1:length(chrs)%%2)+1]
+names(chrColors) <- chrs
+plotdata <- data.frame(
+    CHR,
+    POS,
+    log10P,
+    'plotPos'=NA,
+    'chrNr'= chrNr,
+    pch=20,
+    highlightColor=NA,
+    pcol=chrColors[CHR],
+    check.names=F)
+endPos <- 0
+plotPos <- numeric(0)
+chrLab <- numeric(0)
+chrGAP <- 1E7
+for(chr in chrs){
+    chrTemp <- which(CHR == chr)
+    chrPOS <- POS[chrTemp]-min(POS[chrTemp],na.rm=T)+endPos+1
+    chrLab <- c(chrLab,mean(chrPOS,na.rm=T))
+    endPos <- max(chrPOS,na.rm=T)+chrGAP
+    plotPos <- c(plotPos,chrPOS)
+}
+plotdata$plotPos <- plotPos
+
+# Highlight candidate regions
+if(dim(candidateRegions)[1]>0){
+	a <- 0
+	while(a < dim(candidateRegions)[1]){
+		a <- a + 1 
+		overlap <- which(plotdata$CHR == candidateRegions$CHROM[a] &
+						 plotdata$POS >= candidateRegions$START[a] &
+						 plotdata$POS <= candidateRegions$END[a] &
+						 is.na(plotdata$highlightColor)
+						)
+		if(length(overlap)==0) next
+		plotdata$highlightColor[overlap] <- candidateRegions$COL[a]
+		plotdata$pcol[overlap] <- NA
+	}
+}
+
+# Manhattan plot
+png(filename = paste0(opt$prefix,"_Manhattan.png"), width = opt$mh.width, height = opt$mh.height, pointsize = opt$pointsize)
+    par(mar=c(5.1,5.1,4.1,1.1),las=1)
+    x = plotdata$plotPos
+    y = plotdata$log10P
+    maxY <- max(y,na.rm=T)
+
+	# Version with two y axes
+    if(maxY > opt$break.top){
+        # Manhattan plot with two different y axis scales
+
+        # set axis labels of both scales
+        lab1 <- pretty(c(0,opt$break.top),n=ceiling(12 * (1-opt$top.size)))
+        lab1 <- c(lab1[lab1 < opt$break.top],opt$break.top)
+        lab2 <- pretty(c(opt$break.top,maxY),n=max(3,floor(12 * opt$top.size)))
+        lab2 <- lab2[lab2 > max(lab1)]
+
+        # resulting range of top scale in bottom scale units
+        top.range = opt$break.top/(1 - opt$top.size) - opt$break.top
+        top.data = max(lab2)-opt$break.top
+        # function to rescale the top part
+        rescale = function(y) {opt$break.top+(y-opt$break.top)/(top.data/top.range)}
+
+        # plot bottom part / rescaled
+        plot(x[y<opt$break.top],y[y<opt$break.top],ylim=c(0,opt$break.top+top.range),axes=FALSE,
+            pch=plotdata$pch[y<opt$break.top], cex=0.9,cex.lab=1.5,cex.axis=1.5, xaxt="n",
+            col=plotdata$pcol[y<opt$break.top], ylab=expression(-log[10]*italic(P)), xlab="",bty="n",
+            main=gsub("_"," ",paste0(opt$maintitle,"\n",format(Nmarkers,big.mark=",",scientific=F),
+            " variants")))
+        # plot top part
+        points(x[y>opt$break.top],rescale(y[y>opt$break.top]),pch=plotdata$pch[y>opt$break.top],
+            col=plotdata$pcol[y>opt$break.top],cex=0.9)
+
+        # plot highlighted regions
+        for(hcol in unique(plotdata$highlightColor)){
+            topDot <- plotdata[which(plotdata$highlightColor == hcol & y>opt$break.top),]
+            if(length(topDot)>0) {
+                points(topDot$plotPos,rescale(topDot$log10P),pch=20,col=hcol, cex=0.9)
+            }
+            bottomDot <- plotdata[which(plotdata$highlightColor == hcol & y<=opt$break.top),]
+            if(length(bottomDot)>0) {
+                points(bottomDot$plotPos,bottomDot$log10P,pch=20,col=hcol, cex=0.9)
+            }
+        }
+
+        # add axes and axis labels
+        axis(1,at=chrLab[seq(1,length(chrLab),by=2)],
+            labels=chrs[1:length(chrLab)][seq(1,length(chrLab),by=2)],
+            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=2)
+        axis(1,at=chrLab[seq(2,length(chrLab),by=2)],
+            labels=chrs[1:length(chrLab)][seq(2,length(chrLab),by=2)],
+            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=0)
+        axis(side=2,at=lab1,cex.axis=1.5,cex.lab=1.5)
+        axis(side=2,at=rescale(lab2),labels=lab2,cex.axis=1.5,cex.lab=1.5)
+
+        # plot axis breaks and indicate line of axis break
+        box()
+        axis.break(axis=2,breakpos=opt$break.top,style="zigzag",brw=0.02)
+        axis.break(axis=4,breakpos=opt$break.top,style="zigzag",brw=0.02)
+        abline(h=opt$break.top,lwd=1.5,lty=2,col="grey")
+        if(length(yLine)>0) {
+            for(rl in 1:length(yLine)){
+                if(yLine[rl] <= opt$break.top) {
+                    abline(h=yLine[rl],lwd=1.5,col=colLine[rl],lty=2)
+                } else {
+                    abline(h=rescale(yLine[rl]),lwd=1.5,col=colLine[rl],lty=2)
+                }
+            }
+        }
+	# Version with one y axis / no break in axis
+    } else {
+        plot(x,y,xaxt="n",pch=plotdata$pch,cex=0.9,cex.lab=1.5,cex.axis=1.5,xaxt="n",
+            col=plotdata$pcol,ylab=expression(-log[10]*italic(P)),xlab="",bty="o",
+            main=gsub("_"," ",paste0(opt$maintitle,"\n",format(Nmarkers,big.mark=",",scientific=F),
+            	" variants")),
+            ylim=c(0,ceiling(max(maxY+1,yLine))))
+        axis(1,at=chrLab[seq(1,length(chrLab),by=2)],
+            labels=chrs[1:length(chrLab)][seq(1,length(chrLab),by=2)],
+            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=2)
+        axis(1,at=chrLab[seq(2,length(chrLab),by=2)],
+            labels=chrs[1:length(chrLab)][seq(2,length(chrLab),by=2)],
+            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=0)
+        # plot highlighted regions on top
+        for(hcol in unique(plotdata$highlightColor)){
+            extraDot <- plotdata[which(plotdata$highlightColor == hcol),]
+            points(extraDot$plotPos,extraDot$log10P,pch=20,col=hcol, cex=0.9)
+        }
+        # genome-wide significance linn
+        if(length(yLine)>0) abline(h=yLine,lwd=1.5,col=colLine,lty=2)
+    }
+
+	# Add legend if candidate regions are present 
+    ltext <- unique(candidateRegions[,c("COL","LEGENDTEXT")])
+    if(dim(ltext)[1]>0) legend("topleft",legend=ltext$LEGENDTEXT,col=ltext$COL,pch=15,bty="n")
+dev.off()
---
+++ QQplot.r
@@ -0,0 +1,196 @@
+options(stringsAsFactors=F)
+library("plotrix")
+library("data.table")
+library("RColorBrewer")
+library("optparse")
+
+# QQ plot function
+qqplotdata <- function(logpvector){
+    o = sort(logpvector,decreasing=T)
+    e = -log10(ppoints(length(o)))       
+    qqdata <- data.frame(o,e)
+    qqdata$o <- round(qqdata$o,3)
+    qqdata$e <- round(qqdata$e,3)
+    keepU <- which(!duplicated(qqdata))
+    qqdata <- qqdata[keepU,]
+    
+    N <- length(logpvector) ## number of p-values
+    ## create the confidence intervals
+    qqdata$c95 <- NA
+    qqdata$c05 <- NA
+
+    ## the jth order statistic from a
+    ## uniform(0,1) sample
+    ## has a beta(j,n-j+1) distribution
+    ## (Casella & Berger, 2002,
+    ## 2nd edition, pg 230, Duxbury)
+
+    for(i in 1:length(keepU)){
+        j <- keepU[i]
+        qqdata$c95[i] <- -log10(qbeta(0.95,j,N-j+1))
+        qqdata$c05[i] <- -log10(qbeta(0.05,j,N-j+1))
+    }
+    return(qqdata)
+}
+
+
+option_list <- list(
+  make_option("--input", type="character", default="",
+    help="Input file, tab delimited"),   
+  make_option("--prefix", type="character", default="",
+    help="Prefix of output files"),   
+  make_option("--top.size", type="numeric", default=0.125,
+    help="top size = proportion of total length y axis [default=0.125]"),
+  make_option("--break.top", type="numeric", default=15,
+    help="set axis break at -log10(P) [default=15]"),
+  make_option("--width", type="numeric", default=900,
+    help="Width Manhattan plot in pixel [default=900]"),
+  make_option("--height", type="numeric", default=900,
+    help="Height Manhattan plot in pixel [default=900]"),
+  make_option("--pointsize", type="numeric", default=16,
+    help="Point size of plots [default=16]"),
+  make_option("--maf", type="character", default="",
+    help="name of column with MAF [default='MAF']"),
+  make_option("--p", type="character", default="",
+    help="name of column with p.value [default='PVALUE']"),
+  make_option("--log10p", type="logical", default=T,
+    help="Input p.value column with -log10(p.value) [default=F]"),    
+  make_option("--maintitle", type="character", default="",
+    help="Plot title")  
+)
+
+parser <- OptionParser(usage="%prog [options]", option_list=option_list)
+
+args <- parse_args(parser, positional_arguments = 0)
+opt <- args$options
+print(opt)
+
+# horizontal lines and corresponding colors
+yLine <- c(-log10(5E-8))
+colLine <- c("red")
+
+gwas <- fread(opt$input)
+
+if(!opt$log10p) {
+	gwas$log10P <- -log10(gwas[[opt$p]])
+	ycol <- "log10P"
+} else {
+	ycol <- opt$p
+}
+
+gwas <- na.omit(data.frame(gwas[,c(opt$maf,ycol),with=F]))
+
+minMAF <- min(gwas[[opt$maf]])
+
+# Determine frequency bins and create variable for binned QQ plot
+freqbins <- c(c(0.5,0.05,0.005,0.001,0)[which(c(0.5,0.05,0.005,0.001,0) > floor(minMAF*1000000)/1000000)],floor(minMAF*1000000)/1000000)
+gwas$freqbin <- cut(gwas[[opt$maf]], freqbins,include.lowest=T)
+freqtable <- table(gwas$freqbin)
+freqtable <- freqtable[order(-as.numeric(gsub("[\\[\\(](.+),.+","\\1",names(freqtable))))]
+freqtable <- freqtable[freqtable > 0]
+
+
+## Generate QQ plot data by frequency bin
+fbin <- character(0)
+fN <- integer(0)
+fx <- numeric(0)
+fy <- numeric(0)
+fcol <- character(0)
+legendcol <- character(0)
+conf <- list()
+allcols <- brewer.pal(4,"Set1")
+for(f in 1:length(freqtable)){
+	fbin <- c(fbin,names(freqtable)[f])
+	fsnps <- which(gwas$freqbin ==names(freqtable)[f])
+	plotdata <- qqplotdata(gwas[[ycol]][fsnps])
+	fN <- c(fN,freqtable[f])
+	fx <- c(fx,plotdata$e)
+	fy <- c(fy,plotdata$o)
+	fcol <- c(fcol,rep(allcols[f],length(plotdata$o)))
+	conf[[f]] <- data.frame('x'=c(plotdata$e,rev(plotdata$e)),
+							'y'=c(plotdata$c95,rev(plotdata$c05)))
+	legendcol <- c(legendcol,allcols[f])
+}
+legendtext <- paste0("MAF=",fbin,"; N SNPs=",format(fN,big.mark=",",scientific=FALSE))
+
+
+## QQ plot by binned frequencies
+png(filename = paste0(opt$prefix,".QQ_Plot.png"), width = opt$width, height = opt$height, pointsize = opt$pointsize)
+	xlim <- c(0,max(fx,na.rm=T))
+	ylim <- c(0,max(fy,na.rm=T))
+	maxY <- max(fy,na.rm=T)
+	par(mar=c(5.1,5.1,4.1,1.1))
+	# plot version with two axes
+	if(maxY > opt$break.top){
+		# create pretty y-axis labels
+        lab1 <- pretty(c(0,opt$break.top),n=ceiling(12 * (1-opt$top.size)))
+        lab1 <- c(lab1[lab1 < opt$break.top],opt$break.top)
+        lab2 <- pretty(c(opt$break.top,maxY),n=max(3,floor(12 * opt$top.size)))
+        lab2 <- lab2[lab2 > max(lab1)]
+
+        # resulting range of top scale in bottom scale units
+        top.range = opt$break.top/(1 - opt$top.size) - opt$break.top
+        top.data = max(lab2)-opt$break.top
+        
+        # function to rescale the top part
+		rescale = function(y) { opt$break.top+(y-opt$break.top)/(top.data/top.range)}
+		rescaled.y = rescale(fy[fy>opt$break.top])
+		plot(0,0,
+			ylim=c(min(fy),opt$break.top*(1+opt$top.size)),xlim=xlim,axes=FALSE,
+			xlab=expression(plain(Expected)~~group("(",-log[10]*italic(P),")")),
+			ylab=expression(plain(Observed)~~group("(",-log[10]*italic(P),")")),
+			cex=1,cex.lab=1.5,cex.axis=1.5,bty="n",col="transparent",
+			main=opt$maintitle,pch=19)
+		
+		# Plot confidence intervals	
+		for(p in 1:length(conf)){
+			polygon(conf[[p]]$'x',ifelse(conf[[p]]$'y'>opt$break.top,rescale(conf[[p]]$'y'),conf[[p]]$'y'),
+				col=grDevices::rgb(t(grDevices::col2rgb(allcols[p])),alpha=50,maxColorValue=255),
+				border = NA)
+		}
+
+		# add points
+		points(fx[fy<opt$break.top],fy[fy<opt$break.top],cex=1,col=fcol[fy<opt$break.top],pch=19)
+
+		# identify line & add axis break
+		lines(xlim,xlim,col="black",lty = 2)
+		axis(1,cex.axis=1.5,cex.lab=1.5)
+		par(las=1)
+		axis(side=2,at=lab1,cex.axis=1.5,cex.lab=1.5)
+		par(las=0)
+		box()
+		par(las=0)
+		points(fx[fy>opt$break.top],rescaled.y,cex=1,col=fcol[fy>opt$break.top],pch=19)
+		par(las=1)
+		axis(side=2,at=rescale(lab2),labels=lab2,cex.axis=1.5,cex.lab=1.5)
+		axis.break(axis=2,breakpos=opt$break.top,style="zigzag",brw=0.02)
+		axis.break(axis=4,breakpos=opt$break.top,style="zigzag",brw=0.02)
+		lines(range(fx),c(opt$break.top,opt$break.top),col = "grey",lty = 6)
+		abline(h=ifelse(yLine<opt$break.top,
+			yLine,
+			rescale(yLine)),
+			col=colLine,lwd=1.5,lty=2)
+		legend("topleft",legend=legendtext,col=legendcol,pch=15,bty="n")
+	# plot version with single y axes
+	} else {
+		par(mar=c(5.1,5.1,4.1,1.1),las=1)
+		axislim <- ceiling(range(xlim,ylim,-log10(gwthreshold)))
+		plot(0,0,
+			ylim=axislim,xlim=xlim,axes=T,
+			xlab=expression(plain(Expected)~~group("(",-log[10]*italic(P),")")),
+			ylab=expression(plain(Observed)~~group("(",-log[10]*italic(P),")")),
+			cex=1,cex.lab=1.5,cex.axis=1.5,col="transparent",
+			main=opt$maintitle,pch=19)
+		# Plot confidence intervals
+		for(p in 1:length(conf)){
+				polygon(conf[[p]]$'x',conf[[p]]$'y',
+					col=grDevices::rgb(t(grDevices::col2rgb(allcols[p])),alpha=50,maxColorValue=255),
+					border = NA)
+		}
+		points(fx,fy,col=fcol,pch=19)
+		# identity line & genome-wide significance line
+		lines(axislim,axislim,col = "grey",lwd=1.5,lty=2)
+		abline(h=yLine,col=colLine,lwd=1.5,lty=2)
+		legend("topleft",legend=legendtext,col=legendcol,pch=15,bty="n")
+	}
+dev.off()
